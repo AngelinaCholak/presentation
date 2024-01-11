42dea955795c66ab9987d8a48ace7a3fb6793dc0
AngelinaCholak <127952572+AngelinaCholak@users.noreply.github.com>
2024-01-11 16:10:58 +0400

    Ваш коментар до змін

--- src/pages/Contacts.jsx
+++ src/pages/Contacts.jsx
@@ -1,7 +1,10 @@
 import React from 'react'
 import css from './Contacts.module.css';
+import image1 from './img/slide4.png';
 export const Contacts = () => {
   return (
-    <div>Contacts</div>
-  )
+    <div className={css.container}>
+      <img className={css.img} src={image1} alt="img" />
+    </div>
+  );
 }
